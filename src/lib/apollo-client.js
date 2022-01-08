bd759108ddd6e6366181dc4b2ddf6eda691f4871
tmrp <33550033+tmrp@users.noreply.github.com>
2022-01-08 21:36:48 +0100

    rewrote import to hopefully result in a successful build

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -1,13 +1,12 @@
-import { HttpLink, ApolloClient, InMemoryCache } from '@apollo/client/core';
-// import Apollo, * as ApolloScope from '@apollo/client/core/index.js';
+import Apollo, * as ApolloScope from '@apollo/client/core';
 
 import { environmentVariables } from './environment-variables';
 
-// const HttpLink = Apollo?.HttpLink || ApolloScope?.HttpLink;
+const HttpLink = Apollo?.HttpLink || ApolloScope?.HttpLink;
 
-// const ApolloClient = Apollo?.ApolloClient || ApolloScope?.ApolloClient;
+const ApolloClient = Apollo?.ApolloClient || ApolloScope?.ApolloClient;
 
-// const InMemoryCache = Apollo?.InMemoryCache || ApolloScope?.InMemoryCache;
+const InMemoryCache = Apollo?.InMemoryCache || ApolloScope?.InMemoryCache;
 
 const link = new HttpLink({
 	uri: environmentVariables.starWarsApi
